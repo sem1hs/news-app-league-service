cb12774683c84799ce53e1096c4e333e0a800146
Semih Şahinoğlu <semihshngl@gmail.com>
2026-01-10 14:15:58 +0300

    dosyalar eklendi

---
+++ src/main/java/com/semihsahinoglu/league_service/LeagueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.league_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semihsahinoglu/league_service/LeagueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.league_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
